817d7a8c11de6273fa60256230974268547cee30
Your Name <you@example.com>
2025-10-09 07:09:32 +0300

    feat: add class and functional components

---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,13 @@
+const FunctionalComponent =() => {
+
+    const title = "Arrow Functional Component";
+    // return <h1 className="text-center text-xl font-bold mt-12">{title}</h1>
+
+    return (
+        <>
+            <h1 className="text-center text-xl font-bold mt-12">{title}</h1>
+        </>
+    )
+}
+
+export default FunctionalComponent;
