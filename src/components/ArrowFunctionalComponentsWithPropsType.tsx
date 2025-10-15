f47875a4b8a294bff85b28dd18f3a9707a489ff8
Your Name <you@example.com>
2025-10-15 08:42:15 +0300

    feat: add an arrow functional component with props

---
+++ src/components/ArrowFunctionalComponentsWithPropsType.tsx
@@ -0,0 +1,26 @@
+type PropA = {
+    title: string;
+
+}
+
+type PropB = {
+    description: string;
+}
+type Props = PropA & PropB;
+//
+// interface Props {
+//     title:string;
+// }
+// interface Props {
+//     description:string;
+// }
+const ArrowFunctionalComponentsWithPropsType = ({title, description}: Props) => {
+    return (
+        <>
+            <h1 className="text-center text-xl font-bold mt-12">{title}</h1>
+            <p className="text-center text-gray-700">{description}</p>
+        </>
+    )
+}
+
+export default ArrowFunctionalComponentsWithPropsType;
